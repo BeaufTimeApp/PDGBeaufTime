97b8e7fa8e4a6ca2ff650ed07f69b74197e4caee
BeaufTimeApp <yonisbdl@icloud.com>
2026-08-13 20:54:04 +0200

    Refresh cache for notifications update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='beauftime-v04-social';
-const ASSETS=['./','./index.html','./style.css?v=4','./app.js?v=4','./manifest.webmanifest','./icon.svg'];
+const CACHE='beauftime-v05-notifications';
+const ASSETS=['./','./index.html','./style.css?v=4','./app.js?v=5','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(e.request.method!=='GET'||u.origin!==location.origin)return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
